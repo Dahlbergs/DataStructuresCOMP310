8e97cb205acea3b4586d3c8fda5621d716dcc21e
Bob Blood <bblood4444@gmail.com>
2015-05-24 20:34:15 -0400

    updated sorting algo

--- lab/one/Bagger.java
+++ lab/one/Bagger.java
@@ -14,57 +14,50 @@ public class Bagger {
     }
 
     void sort() {
-	for(int i = 0; i < groceryList.length; i++) {
-	    if(groceryList[i].getBreakable() > groceryList[i+1].getBreakable() && 
-	       groceryList[i].getSquishable() > groceryList[i+1].getSquishable()) {
-		    //Element swap 
-		    //Element i is stored in temp
-
-		    GroceryItem temp = new GroceryItem(groceryList[i].getName(), 
-						       groceryList[i].getBreakable(),
-						       groceryList[i].getSquishable(),
-						       groceryList[i].getWeight());
-
-		    //Element i transformed into i+1
-		    groceryList[i].setName(groceryList[i+1].getName());
-		    groceryList[i].setBreakable(groceryList[i+1].getBreakable());
-		    groceryList[i].setSquishable(groceryList[i+1].getSquishable());
-		    groceryList[i].setWeight(groceryList[i+1].getWeight());
-		    //Element i+1 is filled with temp which is filled with i
-		    groceryList[i+1].setName(temp.getName());
-		    groceryList[i+1].setBreakable(temp.getBreakable());
-		    groceryList[i+1].setSquishable(temp.getSquishable());
-		    groceryList[i+1].setWeight(temp.getWeight());
-					
-		}
-	    else{
-	    	if(groceryList[i].getSquishable() > groceryList[i+1].getSquishable()){
-			    //Element swap 
-			    //Element i is stored in temp
-
-			    GroceryItem tempTwo = new GroceryItem(groceryList[i].getName(),
-								  groceryList[i].getBreakable(),
-								  groceryList[i].getSquishable(),
-								  groceryList[i].getWeight());
-			    //Element i transformed into i+1
+    	for(int i = 0; i < groceryList.length; i++) {
+    		//temp object to hold element[i] for swapping
+    		GroceryItem tempTwo = new GroceryItem(groceryList[i].getName(),
+				  groceryList[i].getBreakable(),
+				  groceryList[i].getSquishable(),
+				  groceryList[i].getWeight());
+	
+    		if(groceryList[i].getWeight() < groceryList[i+1].getWeight()){
+    			//Swap elements
 			    groceryList[i].setName(groceryList[i+1].getName());
 			    groceryList[i].setBreakable(groceryList[i+1].getBreakable());
 			    groceryList[i].setSquishable(groceryList[i+1].getSquishable());
 			    groceryList[i].setWeight(groceryList[i+1].getWeight());
-			    //Element i+1 is filled with temp which is filled with i
-			    groceryList[i+1].setName(tempTwo.getName());
-			    groceryList[i+1].setBreakable(tempTwo.getBreakable());
-			    groceryList[i+1].setSquishable(tempTwo.getSquishable());
-			    groceryList[i+1].setWeight(tempTwo.getWeight());
-	    	}
-	    }
-	    
-	    System.out.println("Break: " + groceryList[i].getBreakable() + " " + 
-			       "Squish: " + groceryList[i].getSquishable() + " " +
-			       "Weight: " + groceryList[i].getWeight());
-	    i++;
+			    
+			    groceryList[i+1].setName(temp.getName());
+			    groceryList[i+1].setBreakable(temp.getBreakable());
+			    groceryList[i+1].setSquishable(temp.getSquishable());
+			    groceryList[i+1].setWeight(temp.getWeight());
 
-	}
+    		}
+    		else if(groceryList[i].getBreakable() > groceryList[i+1].getBreakable()){
+    			//Swap elements
+			    groceryList[i].setName(groceryList[i+1].getName());
+			    groceryList[i].setBreakable(groceryList[i+1].getBreakable());
+			    groceryList[i].setSquishable(groceryList[i+1].getSquishable());
+			    groceryList[i].setWeight(groceryList[i+1].getWeight());
+			    
+			    groceryList[i+1].setName(temp.getName());
+			    groceryList[i+1].setBreakable(temp.getBreakable());
+			    groceryList[i+1].setSquishable(temp.getSquishable());
+			    groceryList[i+1].setWeight(temp.getWeight());
+    		}
+    		else if(groceryList[i].getSquishable() > groceryList[i+1].getSquishable()){
+    			//Swap elements
+			    groceryList[i].setName(groceryList[i+1].getName());
+			    groceryList[i].setBreakable(groceryList[i+1].getBreakable());
+			    groceryList[i].setSquishable(groceryList[i+1].getSquishable());
+			    groceryList[i].setWeight(groceryList[i+1].getWeight());
+			    
+			    groceryList[i+1].setName(temp.getName());
+			    groceryList[i+1].setBreakable(temp.getBreakable());
+			    groceryList[i+1].setSquishable(temp.getSquishable());
+			    groceryList[i+1].setWeight(temp.getWeight());
+    		}
+    	}
     }
-    
 }
