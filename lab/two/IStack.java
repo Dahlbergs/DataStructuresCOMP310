cc71c20d2eae14a79ec1336577b364b16d5f5510
Yves Sabato <sabatoy@gmail.com>
2015-06-07 05:02:38 -0400

    adding clear, peek, isEmpty operators

--- lab/two/IStack.java
+++ lab/two/IStack.java
@@ -1,24 +1,37 @@
 package lab.two;
 
-/** An interface that defines proper operatins for stack type data structures
- * @author Robert Blood, Sam Dahlberg, Yves Sabato
- */
+/** An interface that defines proper operations for stack type data structures
+ 	@author Robert Blood, Sam Dahlberg, Yves Sabato */
 
 public interface IStack<T> {
 	
 	public int getCurrentSize();
+	
 	/** push to the end of the stack
-	 * @return the current stack size
-	 */
-	public void push(X aValue);
+	 	@return the current stack size */
+	public void push(T aValue);
+	
 	/** pop last elements off stack
-	 * @return the last value of the stack
-	 */
-	public X pop();
+	 	@return the last value of the stack */
+	public T pop();
+	
 	/** get frequency of given value
-	 * @param aValue to find frequency of
-	 * @return number of occurrences
-	 */
-
+	 	@param aValue to find frequency of
+	 	@return number of occurrences */
+	
+	
+	
+	/** retrieve last element of the stack
+	   	@return the last value of the stack or null
+	 	if empty */
+	public T peek();
+	
+	/** checks for empty stack 
+	 	@returns true if stack is empty */
+	public boolean isEmpty();
+	
+	/* remove all entries from stack */
+	public void clear();
+		
 }
 
