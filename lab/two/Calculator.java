a873525dda64996c1b097e5ad648f11e32347993
Sam <dahlbergs@wit.edu>
2015-06-08 02:16:41 -0400

    Calculator now works with basic expressions such as (1+2).  Further work is needed for more complex expressions.

--- lab/two/Calculator.java
+++ lab/two/Calculator.java
@@ -12,14 +12,21 @@ public class Calculator {
     public Calculator() {}
 	
     public void addInput(char element) {
-	System.out.println("Add element");
+	//System.out.println("Add element");
 	inputStack.push(element);
     }
 
     public void stackToString() {
-	while(inputStack.isEmpty()) {
+	while(!inputStack.isEmpty()) {
 	    inputString = inputStack.pop() + inputString;
-	}	
+	}
+
+	System.out.println(inputString);
+    }
+
+    public void processInput() {
+	stackToString();	
+	processParenthesis(inputString);
     }
 
     public String processParenthesis(String str) {
@@ -27,22 +34,25 @@ public class Calculator {
 	String output = "";
 	
 	//Iterates through string looking for parenthesis
+	System.out.println("Looking for parenthesis");
 	for(int i = 0; i < str.length(); i++) {
-	    //if we find an open one, break it into smaller expression
+	    //if we find an open one, break it into smaller expression	    
 	    if(str.charAt(i) == '(' ) {
+		System.out.println("Found open paren");
 		//Iterate through new expression looking for closed parenthesis
 		for(int j = i+1; j < str.length(); j++) {
 		    //If we do not find a close one, add the element to a temp string and continue
 		    if(str.charAt(j) != ')') {
 			temp = temp + str.charAt(j);
 		    } else {  //If we do find one, check that the new substring doesnt have any more parenthesis	
-			System.out.println(temp);
-			processParenthesis(str.substring(j, str.length()));			
+			//System.out.println(temp);
+			processParenthesis(str.substring(j, str.length()));
+			temp = processExpression(temp);
 		    }
 		}
 	    }
-	}	    
-	temp = processExpression(temp) + temp;
+	}	    	
+	System.out.println(temp);
 	return temp;
     }
 
@@ -51,8 +61,7 @@ public class Calculator {
 	//Assert that string is a valid expression
 
 	//While the substring is not smaller than a single expression, evaluate the expression
-	while(str.length() > 2) {
-	    
+	while(str.length() > 2) {	    
 	    String potato = null;
 
 	    char[] tempChar = new String(str.substring(2, str.length() - 1)).toCharArray();
@@ -73,12 +82,15 @@ public class Calculator {
 	        Integer multiply = Character.getNumericValue(str.charAt(0)) *
 		    Character.getNumericValue(str.charAt(2));
 		potato = multiply.toString() + new String(tempChar);
+		break;
 	    case '/':
 		Integer divide = Character.getNumericValue(str.charAt(0)) /
 		    Character.getNumericValue(str.charAt(2));
 		potato = divide.toString() + new String(tempChar);
+		break;
 	default:
 	    System.out.println("Invalid Expression");
+	    System.exit(1);
 	    }
 	    if(potato != null && potato.length() > 2) {
 		System.out.println(potato);
--- lab/two/Main.java
+++ lab/two/Main.java
@@ -11,22 +11,28 @@ public class Main {
     public static void main(String[] args) {
 	Calculator calc = new Calculator();
 	// 1 + 2 * 2 -3 = 2
-	calc.addInput('1');
-	calc.addInput('+');
-	calc.addInput('2');
+
+	calc.addInput('(');
+	calc.addInput('4');
 	calc.addInput('*');
-	calc.addInput('(');
-	calc.addInput('2');
-	calc.addInput('+');
-	calc.addInput('8');
-	calc.addInput('+');
-	calc.addInput('(');
-	calc.addInput('2');
-	calc.addInput('+');
-	calc.addInput('2');
+	calc.addInput('3');	
 	calc.addInput(')');
-	calc.addInput(')');
+	
+	
 
+	// calc.addInput('*');
+	// calc.addInput('(');
+	// calc.addInput('2');
+	// calc.addInput('+');
+	// calc.addInput('8');
+	// calc.addInput('+');
+	// calc.addInput('(');
+	// calc.addInput('2');
+	// calc.addInput('+');
+	// calc.addInput('2');
+	// calc.addInput(')');
+	// calc.addInput(')');
 
+	calc.processInput();
     }
 }
