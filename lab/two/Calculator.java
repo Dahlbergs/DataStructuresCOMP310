383f8c5a8205d77d7fb965f66f34065434123a95
Yves Sabato <sabatoy@gmail.com>
2015-06-08 22:08:39 -0400

    mutli parser

--- lab/two/Calculator.java
+++ lab/two/Calculator.java
@@ -25,26 +25,82 @@ public class Calculator {
     		System.out.println("input stack:" + inputStack.peek());
     	}
     
-    	parseInput();
+    	//parseInput();
     }
-
-    public void parseInput() {        
-
-    System.out.println("parsing input stack:" + inputStack.peek());
-
-	while(!inputStack.isEmpty()) {
-		
-	    if(isOperand(inputStack.peek())) {
-	    	
-		operandStack.push(Character.getNumericValue(inputStack.pop()));
-	    } 
-	    else if(isOperator(inputStack.peek())) {
-	    	
-		operatorStack.push(inputStack.pop());
-	    }
-	}
-	
+   
+  //A method to determine operator precedence.
+    public static int prec(char op){
+        int temp = 0;
+        if (op == '(' || op == ')')
+            temp = 0;
+        else  if (op == '*' || op == '/')
+            temp = 3;
+        else  if (op == '+' || op == '-')
+            temp = 2;  
+        else
+        	temp = 1;
+        return temp;
     }
+  
+    
+    public int parseInput(String expression) {
+    	
+    	int len = expression.length();
+    	char[] input = expression.toCharArray();
+    	
+    	for (int i = 0; i < len; i++){
+    	
+    		if (input[i] == '('){
+                operatorStack.push(input[i]); // add open bracket to operator stack
+                }
+            else if (input[i] == ')'){ // if close bracket has higher value precedence than the operator already in stack AND also if it's not an open bracket then evaluate.
+                while (!operatorStack.isEmpty() &&  prec(operatorStack.peek()) >= prec(input[i]) && input[i] == ')' && operatorStack.peek() != '('){              
+                    processStacks();
+                    if (!operatorStack.isEmpty())
+                        operatorStack.pop(); // if operator stack is not empty, remove operator just used.
+                }
+            }
+            else if (input[i] >= '0' && input[i] <= '9'){	
+            	operandStack.push(Character.getNumericValue(input[i]));
+            }
+            else if (input[i] == '+' || input[i] == '-' || input[i] == '*' || input[i] == '/'){
+            	
+                while (!operatorStack.isEmpty() &&prec(operatorStack.peek()) >= prec(input[i])){   
+                    processStacks();
+                }
+                
+                operatorStack.push(input[i]);
+            }
+     
+    	
+    	}
+    	
+    	while (!operatorStack.isEmpty())
+    		processStacks();
+    	
+    	return operandStack.pop();
+    }
+    
+    
+    
+
+//    public void parseInput() {        
+//
+//    System.out.println("parsing input stack:" + inputStack.peek());
+//
+//	while(!inputStack.isEmpty()) {
+//		
+//	    if(isOperand(inputStack.peek())) {
+//	    	
+//		operandStack.push(Character.getNumericValue(inputStack.pop()));
+//	    } 
+//	    else if(isOperator(inputStack.peek())) {
+//	    	
+//		operatorStack.push(inputStack.pop());
+//	    }
+//	}
+//	
+//    }
     
 //    public void convertToPostfix() {
 //	Stack<Character> postfix = new Stack<Character>();
@@ -101,10 +157,10 @@ public class Calculator {
 //	    System.out.println(inputStack.top());
 //	}
 //    }
+    
 
-    public int processStacks() {
+    public void processStacks() {
     	
-	while(operandStack.getCurrentSize() > 1) {
 		System.out.println("processing operand stack:" + operandStack.peek());
 	    int temp = operandStack.pop();
 	    char temp2 = operatorStack.pop();
@@ -122,14 +178,15 @@ public class Calculator {
 		operandStack.push(temp*temp3);
 		break;
 	    case '/':
+	    	if (temp3 == 0) throw new UnsupportedOperationException ("Cannot divide by zer");
 		operandStack.push(temp/temp3);
 		break;
 	    default:
 		System.out.println("Invalid Operator");
 	    }   
-	}
+	
 		System.out.println("Operand stack or solution is:" + operandStack.peek());
-		return operandStack.peek();
+		//return operandStack.peek();
     }
 
     public boolean isOperand(char character) {
