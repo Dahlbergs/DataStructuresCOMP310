0d9d145339fb6dba340e3178d62c079be3f47b5a
Sam <dahlbergs@wit.edu>
2015-06-15 19:04:38 -0400

    * Add functionality to TrainRoute class and test 	* Test TrainRoute class constructor and printContents

--- lab/three/TrainRoute.java
+++ lab/three/TrainRoute.java
@@ -1,42 +1,55 @@
 package lab.three;
 
 import java.io.*;
+import java.util.*;
 
 public class TrainRoute {
+    
+    private static final String[] DEFAULT_STATION_LIST = {"North Station",
+						  "South Station",
+						  "New York Station",
+						  "Philidelphia Station",
+						  "Washington D.C. Station",
+						  "Baltimore Station",
+						  "Richmond Station",
+						  "Louisville Station",
+						  "Savannah Station",
+						  "Jacksonville Station"};
+
+    private ArrayList<TrainStation> trainStations;
+    private TrainStation firstStation;
+    private TrainStation lastStation;
+
+    public TrainRoute() {
+	trainStations = new ArrayList<TrainStation>(DEFAULT_STATION_LIST.length);
+	
+	firstStation = new TrainStation(DEFAULT_STATION_LIST[0]);
+        lastStation = new TrainStation(DEFAULT_STATION_LIST[DEFAULT_STATION_LIST.length - 1]);
+
+	for(int i = 0; i < DEFAULT_STATION_LIST.length; i++) {
+	    trainStations.add(new TrainStation(DEFAULT_STATION_LIST[i]));
+	} 
+    }
+
+    public TrainRoute(String[] stations) {
+	trainStations = new ArrayList<TrainStation>(stations.length);
+
+	firstStation = new TrainStation(stations[0]);
+        lastStation = new TrainStation(stations[stations.length - 1]);
+
+	for(int i = 0; i < stations.length; i++) {
+	    trainStations.add(new TrainStation(stations[i]));
+	}
+    }
+
+    public void printContents() {
+	for(int i = 0; i < trainStations.size(); i++) {
+	    System.out.println(trainStations.get(i).getStationName());
+	}
+    }
 
     public static void main(String args[]) {
-	File inFile = null;
-	
-	if (0 < args.length) {
-	    inFile = new File(args[0]);
-	} else {
-	    System.out.println("Invalid Argument, Exiting");
-	    return;
-	}
-	
-	BufferedReader br = null;
-	
-	try {	    
-	    String sCurrentLine;
-	    
-	    br = new BufferedReader(new FileReader(inFile));
-	    
-	    while ((sCurrentLine = br.readLine()) != null) {
-		System.out.println(sCurrentLine);
-	    }
-	    
-	} 
-
-	catch (IOException e) {
-	    e.printStackTrace();
-	} 
-	
-	finally {
-	    try {
-		if (br != null)br.close();
-	    } catch (IOException ex) {
-		ex.printStackTrace();
-	    }
-	}
+	TrainRoute route = new TrainRoute();
+	route.printContents();
     }
 }
--- lab/three/TrainStation.java
+++ lab/three/TrainStation.java
@@ -1,22 +1,30 @@
 package lab.three;
 
 public class TrainStation {
-    private static final String DEFAULT_STATION_NAME = "Default Station";
-    private static final int MAX_CAPACITY = 10;
-    
+    private static final String[] DEFAULT_STATION_LIST = {"North Station",
+						  "South Station",
+						  "New York Station",
+						  "Philidelphia Station",
+						  "Washington D.C. Station",
+						  "Baltimore Station",
+						  "Richmond Station",
+						  "Louisville Station",
+						  "Savannah Station",
+						  "Jacksonville Station"};
+
+
+    //private static final String DEFAULT_STATION_NAME = "Default Station";
+    private static final int MAX_CAPACITY = 10;    
+
     private String stationName;
     private ArrayQueue<Passenger> passengers = new ArrayQueue<Passenger>();
     
     public TrainStation() {
-	this.stationName = DEFAULT_STATION_NAME;
+	this.stationName = "Default Station";
     }
 
-    public TrainStation(String name, double[] schedule) {
+    public TrainStation(String name) {
 	this.stationName = name;
-    }
-
-    public void generatePassengers() {
-	
     }
 
     public String getStationName() {
