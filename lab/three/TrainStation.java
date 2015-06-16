801b157ddd3b7aa3695c3036db357053cc6a5aaf
Bob Blood <bblood4444@gmail.com>
2015-06-16 10:07:10 -0400

    train stuff

--- lab/three/TrainStation.java
+++ lab/three/TrainStation.java
@@ -15,15 +15,50 @@ public class TrainStation {
 	this.stationName = name;
     }
 
-    public void generatePassengers() {
-	
+    public void generatePassenger() {
+        int iGen2 = new Random().nextInt(passengerManifest.length);
+	    String randomPassenger = (passengerManifest[iGen2]);
+    }
+
+    public void generateStation(){
+        int iGen1 = new Random().nextInt(DEFAULT_STATION_LIST.length);
+        String randomDestination = (DEFAULT_STATION_LIST[iGen1]);
     }
 
     public String getStationName() {
-	return this.stationName;
+	   return this.stationName;
     }
 
     public void setStationName(String name) {
-	this.stationName = name;
+	   this.stationName = name;
+    }
+
+    public TicketGenerator(generatePassenger().getPassenger(), generateStation().getStationName(), generateStation().getStationName()){
+        String Passenger = generatePassenger().getPassenger();
+        String Boarding = generateStation().getStationName();
+        String Destination = generateStation().getStationName();
+    }
+
+    private static final String[] DEFAULT_STATION_LIST = {"North Station",
+                          "South Station",
+                          "New York Station",
+                          "Philidelphia Station",
+                          "Washington D.C. Station",
+                          "Baltimore Station",
+                          "Richmond Station",
+                          "Louisville Station",
+                          "Savannah Station",
+                          "Jacksonville Station"};
+
+    //creates random passenger/source/destination itinerary
+
+
+
+
+
+    while(i > 10){
+        TicketGenerator temp = new TicketGenerator(randomPassenger, randomBoard, randomDestination);
+        i++;
+        System.out.println(temp);
     }
 }
